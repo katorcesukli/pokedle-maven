dc75ae96b474ddca1d1db57e4bb08b36c5d2df46
restrellado <rianestrellado3@gmail.com>
2026-03-04 08:06:31 +0800

    (front end): url updated

--- pokedle_frontend/src/App.js
+++ pokedle_frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-const API_BASE = "http://localhost:8080/api/v1/game";
+const API_BASE = "http://192.168.11.34:8080/api/v1/game";
 
 function App() {
   const [hint, setHint] = useState('');
